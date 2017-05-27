560c089b27ee33e7b3cafa1b0b6e1fed0b59db16
mvelasco93 <m_velasco93@hotmail.com>
2017-05-26 23:50:39 -0500

    WIP rbac model create tables

--- sql-db/20170526-v1.sql
+++ sql-db/20170526-v1.sql
@@ -4,35 +4,82 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `rol` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `doctor` (
+  `id_doctor` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_doctor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `ejercicio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ejercicio` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `ejercicio` (
+  `id_ejercicio` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `descripcion` mediumtext,
+  `id_extremidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_ejercicio`),
+  KEY `FK_ejercicio_extremidad` (`id_extremidad`),
+  CONSTRAINT `FK_ejercicio_extremidad` FOREIGN KEY (`id_extremidad`) REFERENCES `extremidad` (`id_extremidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `extremidad` DISABLE KEYS */;
-/*!40000 ALTER TABLE `extremidad` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `extremidad` (
+  `id_extremidad` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_extremidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_admin_grupo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_admin_grupo` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_admin_grupo` (
+  `id_admin` int(11) DEFAULT NULL,
+  `id_grupo` int(11) DEFAULT NULL,
+  KEY `FK_rbac_admin_grupo_admins` (`id_admin`),
+  KEY `FK_rbac_admin_grupo_rbac_grupo` (`id_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_grupo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_grupo` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_grupo` (
+  `id_grupo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_grupo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_grupo_rol` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_grupo_rol` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_grupo_rol` (
+  `id_grupo` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  PRIMARY KEY (`id_grupo`,`id_rol`),
+  KEY `FK_rbac_grupo_rol_rbac_rol` (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_permiso` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_permiso` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_permiso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_rol` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_rol` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_rol` (
+  `id_rol` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*!40000 ALTER TABLE `rbac_rol_permiso` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rbac_rol_permiso` ENABLE KEYS */;
+CREATE TABLE IF NOT EXISTS `rbac_rol_permiso` (
+  `id_rol` int(11) NOT NULL,
+  `id_permiso` int(11) NOT NULL,
+  PRIMARY KEY (`id_rol`,`id_permiso`),
+  KEY `FK_rbac_rol_permiso_rbac_permiso` (`id_permiso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
