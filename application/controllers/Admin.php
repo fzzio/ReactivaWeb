e0dd556d2b1b074cda16d8658eb82b8de37eebf3
Made Velasco <m_velasco93@hotmail.com>
2017-06-05 01:18:27 -0500

    Sprint 2 - Tasks 135, 136, 137, 138 CRUD Administradores

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -53,6 +53,82 @@ class Admin extends CI_Controller{
 		}
 	}
 
+
+	/* CRUD Starts*/
+	public function admins(){
+		$debug = false;
+
+		if ($this->AdminSecurityCheck()){
+            $titulo = "Administradores";
+
+            $crud = new grocery_CRUD();
+			$crud->set_table("acc_admin");
+			$crud->set_subject( $titulo );
+
+			$crud->display_as( 'name' , 'Nombres' );
+			$crud->display_as( 'lastname' , 'Apellidos' );
+			$crud->display_as( 'username' , 'Usuario' );
+			$crud->display_as( 'email' , 'Correo' );
+			$crud->display_as( 'password' , 'Contraseña' );
+			$crud->display_as( 'last_ip' , 'Última IP' );
+			$crud->display_as( 'last_login' , 'Última Conexión' );
+			$crud->display_as( 'status' , 'Estado' );
+
+			$crud->field_type('status', 'dropdown', array(
+                '0' => 'Inactivo',
+                '1' => 'Activo'
+            ));
+
+			$crud->field_type('last_login', 'readonly');
+			$crud->field_type('last_ip', 'readonly');
+
+			$crud->callback_edit_field('password',array($this,'set_password_input_to_empty'));
+            $crud->callback_add_field('password',array($this,'set_password_input_to_empty'));
+
+            $crud->field_type('password','password');
+
+            $crud->callback_before_update(array($this,'encrypt_pw'));
+            $crud->callback_before_insert(array($this,'encrypt_pw'));
+
+			$crud->columns( 'name', 'lastname', 'username', 'email', 'last_ip', 'last_login', 'status' );
+			$crud->fields( 'name', 'lastname', 'username', 'email', 'password', 'status');
+			$crud->required_fields( 'name', 'lastname', 'username', 'email', 'status'  );
+
+            $crud->unset_export();
+			$crud->unset_print();
+			$crud->unset_read();
+
+			$output = $crud->render();
+
+			$dataHeader['PageTitle'] = $titulo;
+			$dataHeader['css_files'] = $output->css_files;
+			$dataFooter['js_files'] = $output->js_files;
+			$dataContent['debug'] = $debug;
+
+            $data['header'] = $this->load->view('admin/header', $dataHeader);
+			$data['menu'] = $this->load->view('admin/menu', $dataHeader );
+
+			$data['content'] = $this->load->view('admin/blank', $output);
+			$data['footer'] = $this->load->view('admin/footer-gc', $dataFooter);
+		}else{
+			redirect("admin/login");
+		}
+	}
+
+	
+
+	/* CRUD ends*/
+	/* Helpers starts*/
+	function AdminSecurityCheck(){
+		$UserAdmin = new UserAdmin();
+		$user = $this->session->userdata('Mail');
+		if ($user){
+			return true;
+		}else{
+			return false;
+		}
+	}	
+
 	public function authenticate(){
 		$username = $this->input->post("ra_username");
 		$password = $this->input->post("ra_password");
@@ -70,14 +146,17 @@ class Admin extends CI_Controller{
         }
 	}
 
-	function AdminSecurityCheck(){
-		$UserAdmin = new UserAdmin();
-		$user = $this->session->userdata('Mail');
-		if ($user){
-			return true;
-		}else{
-			return false;
-		}
+	function encrypt_pw($post_array, $pk) {
+		if(!empty($post_array['password'])) {
+            $post_array['password'] = md5($post_array['password']);
+        }else{
+        	unset($post_array['password']);
+        }
+        return $post_array;
+	}
+
+	function set_password_input_to_empty() {
+		return "<input type='text' name='password' value='' />";
 	}
 }
 
