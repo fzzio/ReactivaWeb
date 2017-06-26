49cb8a8bfd154fe1f3a9c8d738c3524e53ce4a1d
fndos <fndos@hotmail.com>
2017-06-25 23:42:56 -0500

    Sprint 4 Task 302 303 Patient
    patient view, not ready yet

--- application/views/web/footer.php
+++ application/views/web/footer.php
@@ -1,12 +1,11 @@
 		</div>
+		<footer class="col-lg-10">
+			<small>REACTIVA 2017 &copy; Todos los derechos reservados.</small>
+			<img id="osa" src="../../images/rea-footer.png" alt="osa">
+		</footer>
 	</body>
-	<footer class="footer-distributed">
-
-
-
+	
 
 		<script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-	</footer>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </html>
