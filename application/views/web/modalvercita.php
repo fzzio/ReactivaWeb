08add42f467d98e3bf3d7c86b8dfcd814332c8f5
israel1x <israel.5k@hotmail.com>
2017-07-04 00:13:20 -0500

    Sprint 6 Task 325 - 326

---
+++ application/views/web/modalvercita.php
@@ -0,0 +1,84 @@
+<div class="elemntofocus">
+	<a href="#" class="list-group-item" data-toggle="modal" data-target="#myModal">
+		<h4 class="list-group-item-heading">List group item heading 5</h4>
+		<p class="list-group-item-text">...</p>
+	</a>
+</div>
+
+
+ <div class="modal fade" tabindex="-1" id="myModal" role="dialog" >
+		  	<div class="modal-dialog" role="document" id="modalcitadeldiax">
+		  		<div class="modal-content">
+		  			<div class="modal-header" id="colorheadermodal">
+		  				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		  				<h4 class="modal-title" id="textmodalcolor">CITA DEL DIA</h4>
+		  			</div>
+		  			<div class="modal-body" id="modalbodycontenedor">
+		  				<div class="form-horizontal">
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Paciente:</label>
+		  							<p class="datoscitasmf"> Israel zurita</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Hora cita:</label>
+		  							<p class="datoscitasmf"> 9:30 - 10:30</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Sexo:</label>
+		  							<p class="datoscitasmf"> masculino</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Fecha nac:</label>
+		  							<p class="datoscitasmf"> 20/09/2017</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Ced/RUC:</label>
+		  							<p class="datoscitasmf"> 0988829914</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Celular:</label>
+		  							<p class="datoscitasmf"> 0988829914</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">Email:</label>
+		  							<p class="datoscitasmf"> izurita@espol.edu.ec</p>
+		  						</div>
+		  					</div>
+		  					<div class="form-grounp">
+		  						<div class="rectificar">
+		  							<label class="tituloscitas">No de cita:</label>
+		  							<p class="datoscitasmf"> 2</p>
+		  						</div>
+		  					</div>
+		  					<div>
+		  					<!-- <div class="col-sm-6 col-md-6"> -->
+								<label class="tituloscitas">Enfermedades</label>
+								<textarea class="form-control selectfontc textareacitam" rows="2">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500
+								</textarea>
+							<!-- </div> -->
+		  					</div>
+		  				</div>
+		  				
+
+		  			</div>
+		  			<div class="modal-footer" id="footermodalvcita">
+		  				<button type="button" class="btn btn-default" id="btnvcitainiciar"><span class=" glyphicon glyphicon-briefcase" aria-hidden="true"></span> INICIAR CITA</button>
+		  				<button type="button" class="btn btn-default" id="btnvcitaagendar"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> REAGENDAR</button>
+		  				<button type="button" class="btn btn-default" id="btnvcitacancelar" data-dismiss="modal"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> CANCELAR</button>
+		  			</div>
+		  		</div><!-- /.modal-content -->
+		  	</div><!-- /.modal-dialog -->
+		  </div><!-- /.modal -->
