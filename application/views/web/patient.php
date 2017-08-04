5cc1d9ae9554fda9c863ff014a96adcb78db7f43
Madelyne Velasco <m_velasco93@hotmail.com>
2017-08-04 13:51:18 -0500

    S8 T311 View patient corrections

--- application/views/web/patient.php
+++ application/views/web/patient.php
@@ -1,8 +1,9 @@
 <!-- Page Content -->
-<div id="page-wrapper" class="page-patient">
-	<div class="col-md-11">
-		<h3 class="title">Datos del Paciente</h3>
-		<div class="row">
+<div id="page-wrapper" class="page-main mt-0 pt-0 mb-0 pb-0 pr-0 mr-0">
+
+	<div class="col-md-offset-1 col-md-10 ">
+		<h2 class="title">Datos del Paciente</h2>
+		<div class="row pt-20">
 			<div class="col-md-2">
 				<a type="button" class="btn btn-success btn-general mb-10" href="<?php echo site_url('web/editarPaciente/').$paciente->getId(); ?>"><span class="glyphicon glyphicon-pencil"></span>Editar datos</a>
 				<a type="button" class="btn btn-danger btn-general mb-10" href="<?php echo site_url('web/eliminarPaciente/').$paciente->getId(); ?>"><span class="glyphicon glyphicon-remove"></span>Eliminar</a>
@@ -41,6 +42,14 @@
 						<p><span class = 'patient-content'><?php echo $paciente->getBorn() ?></span> </p>
 					</div>
 				</div>
+				<div class = 'row'>
+					<div class = 'col-xs-6'>
+						<p><span class = 'patient-label'>Contacto de emergencia:</span></p>
+					</div>
+					<div class = 'col-xs-6'>
+						<p><span class = 'patient-content'><?php echo $paciente->getEmergency_contact() ?></span> </p>
+					</div>
+				</div>
 			</div>
 			<div class = 'col-md-5'>
 				<div class = 'row'>
@@ -75,40 +84,49 @@
 						<p><span class = 'patient-content'><?php echo $paciente->getAddress() ?></span> </p>
 					</div>
 				</div>
+				<div class = 'row'>
+					<div class = 'col-xs-6'>
+						<p><span class = 'patient-label'>Teléfono de emergencia:</span></p>
+					</div>
+					<div class = 'col-xs-6'>
+						<p><span class = 'patient-content'><?php echo $paciente->getEmergency_phone() ?></span> </p>
+					</div>
+				</div>
 			</div>
 		</div>
-		<h4 class="sub-title">HISTORIAL DE CITAS Y TERAPIAS</h3>
+		<h4 class="sub-title pt-15">HISTORIAL DE CITAS Y TERAPIAS</h4>
 		<table class="table consult">
-	    <thead>
-	      <tr>
-	      	<th></th>
-	        <th>TIPO</th>
-	        <th style="width: 100px;">FECHA</th>
-	        <th>DOCTOR/TERAPEUTA</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	      <tr>
-	      	<td>
-	      		<button type="button" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-eye-open"></span></button>
-	      	</td>
-	        <td>Consulta</td>
-	        <td>16-06-2017</td>
-	        <td>Daniel García</td>
-	       
-	      </tr>
-	      <tr>
-	      	<td>
-	      		<button type="button" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-eye-open"></span></button>
-	      	</td>
-	        <td>Terapia</td>
-	        <td>01-08-2017</td>
-	        <td>Erick Cedeño</td>
+			<thead>
+				<tr class = 'mt-0 pt-0'>
+					<th></th>
+					<th>TIPO</th>
+					<th>FECHA</th>
+					<th>DOCTOR/TERAPEUTA</th>
+				</tr>
+			</thead>
+			<tbody>
+			  <tr>
+			  	<td>
+			  		<button type="button" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-eye-open"></span></button>
+			  	</td>
+			    <td class = 'left-cell'>Consulta</td>
+			    <td>16-06-2017</td>
+			    <td>Daniel García</td>
+			   
+			  </tr>
+			  <tr>
+			  	<td>
+			  		<button type="button" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-eye-open"></span></button>
+			  	</td>
+			    <td class = 'left-cell'>Terapia</td>
+			    <td>01-08-2017</td>
+			    <td>Erick Cedeño</td>
 
-	      </tr>
-	    </tbody>
-	  </table>
-		<h4 class="sub-title">Información clínica</h3>
+			  </tr>
+			</tbody>
+		</table>
+		<h4 class="sub-title pt-15">Información clínica</h3>
+
 		<div class="row">
 			<div class = 'col-md-4'>
 				<div class = 'row'>
@@ -116,7 +134,7 @@
 						<p><span class = 'patient-label'>Sexo:</span></p>
 					</div>
 					<div class = 'col-xs-6'>
-						<p><span class = 'patient-content'><?php echo $paciente->getGender() ?> </span></p>
+						<p><span class = 'patient-content'><?php if($paciente->getGender()== 0){ echo "Femenino";}else{ echo "Masculino";} ?> </span></p>
 					</div>
 				</div>
 				<div class = 'row'>
@@ -132,7 +150,7 @@
 						<p><span class = 'patient-label'>Alergias:</span></p>
 					</div>
 					<div class = 'col-xs-6'>
-						<p><span class = 'patient-content'><?php echo $paciente->getAllergies() ?></span> </p>
+						<p><span class = 'patient-content'><?php echo $paciente->getAllergies_med() ?></span> </p>
 					</div>
 				</div>
 			</div>
@@ -140,10 +158,8 @@
 			<div class="col-md-5">
 				<h5 class="title-3">Observaciones</h5>
 				<p class = 'p-5 patient-input'><?php echo $paciente->getObservations() ?></p>
-				<h5 class="title-3">Alergias</h5>
+				<h5 class="title-3">Enfermedades</h5>
 				<p class = 'p-5 patient-input'><?php echo $paciente->getAllergies() ?></p>
-				<h5 class="title-3">Observaciones</h5>
-				<p class = 'p-5 patient-input'><?php echo $paciente->getObservations() ?></p>
 			</div>
 		</div>
 	</div>
