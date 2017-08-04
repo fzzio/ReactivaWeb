e04d945f79cf247615a955a0a5bbc4418a90c5ee
Madelyne Velasco <m_velasco93@hotmail.com>
2017-08-04 02:23:38 -0500

    Patient list corrections

--- application/views/web/paciente-lista.php
+++ application/views/web/paciente-lista.php
@@ -1,9 +1,9 @@
 <!-- Page Content -->
 <div id="page-wrapper" class="page-main mt-0 pt-0 mb-0 pb-0 pr-0 mr-0">
 
-<div class="col-md-10 ">
-	<h3 class="title mayusc">Administrar pacientes</h3>
-	<a type="button" class="btn btn-nuevo" href="<?php echo site_url('web/nuevoPaciente'); ?>">
+<div class="col-md-offset-1 col-md-10 ">
+	<h2 class="title mayusc">Administrar pacientes</h2>
+	<a type="button" class="btn btn-nuevo mt-10" href="<?php echo site_url('web/nuevoPaciente'); ?>">
 		<div class = 'glyphicon-ring'>
 			<span class="glyphicon glyphicon-plus glyphicon-bordered" ></span>
 		</div>
@@ -14,11 +14,11 @@
 		<thead>
 			<tr>
 				<th></th>
-				<th>NOMBRES</th>
-				<th>APELLIDOS</th>
-				<th>EDAD</th>
-				<th>CÉDULA</th>
-				<th>EMAIL</th>
+				<th class = 'text-center'>NOMBRES</th>
+				<th class = 'text-center'>APELLIDOS</th>
+				<th class = 'text-center'>EDAD</th>
+				<th class = 'text-center'>CÉDULA</th>
+				<th class = 'text-center'>EMAIL</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -29,11 +29,11 @@
 					<a type="button" class="btn btn-xs btn-success" href="<?php echo site_url('web/editarPaciente/').$pax['id_patient']; ?>"><span class="glyphicon glyphicon-pencil"></span></a>
 					<button type="button" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-remove"></span></button>
 				</td>
-				<td><?php echo $pax['name'];?></td>
-				<td><?php echo $pax['lastname'];?></td>
-				<td><?php echo $pax['born'];?></td>
-				<td><?php echo $pax['ci'];?></td>
-				<td><?php echo $pax['email'];?></td>
+				<td class = 'text-center left-cell'><?php echo $pax['name'];?></td>
+				<td class = 'text-center'><?php echo $pax['lastname'];?></td>
+				<td class = 'text-center'><?php echo $pax['born'];?></td>
+				<td class = 'text-center'><?php echo $pax['ci'];?></td>
+				<td class = 'text-center'><?php echo $pax['email'];?></td>
 			</tr>
 			<?php } ?>
 		</tbody>
