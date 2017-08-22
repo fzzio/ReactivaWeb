9f1e738281711ab8f9702c0845a17256d845c20b
fndos <fndos@hotmail.com>
2017-08-22 01:04:58 -0500

    Testing AdminController functionality
    http://localhost/ReactivaWeb/index.php/testing

---
+++ application/controllers/Testing.php
@@ -0,0 +1,39 @@
+<?php 
+
+if (!defined('BASEPATH')) exit ("No direct script access allowed");
+
+class Testing extends CI_Controller {
+
+	public function __construct() {
+		parent:: __construct();
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->library('unit_test');
+		$this->load->model('User');
+		date_default_timezone_set("America/Guayaquil");
+	}
+
+	public function index() {
+		//Initialize User
+		$user = new User();
+
+		//Testingo login function
+		$this->unit->run($user->login('mvelasco', 'admin'), true, 'Testing login function');
+		//Testing getID function
+		$this->unit->run($user->getID('mvelasco'), 1, 'Testing getID function');
+		//Testing getGroup function
+		$this->unit->run($user->getGroup('mvelasco'), 1, 'Testing getGroup function');
+		//Testing getMail function
+		$this->unit->run($user->getMail('mvelasco'), 'madelyne@cajanegra.com.ec', 'Testing getMail function');
+		//Testing getName function
+		$this->unit->run($user->getName('mvelasco'), 'Madelyne Velasco', 'Testing getName function');
+		//Testing getPermission function
+		$this->unit->run($user->getPermission(1, 1), true, 'Testing getPermission function');
+
+		//Unit test report
+		echo $this->unit->report();
+
+
+
+	}
+}
