2b6409211c9251866b7b0ecd2356f429221765f4
Ing. Henry Lomas A <henry@cajanegra.com.ec>
2017-12-27 08:04:31 -0500

    Pricing

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -152,14 +152,14 @@
 							<li>
 								<a href="#description">Descripción</a>
 							</li>
-							<?php /*
+							
 							<li>
 								<a href="#testimonials">Testimonios</a>
 							</li>
 							<li>
-								<a href="#pricing">Precio</a>
+								<a href="#pricing">Inversión</a>
 							</li>
-							*/ ?>
+							
 						</ul>
 					</div>
 				</div>
@@ -665,7 +665,7 @@
 			</section>
 			*/ ?>
 
-			<?php /*
+			
 			<!--
 			=================================
 			PRICING SECTION
@@ -846,7 +846,7 @@
 					</ul>
 				</div>
 			</section>
-			*/ ?>
+			
 
 			<!--
 			=================================
